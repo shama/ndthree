b12b249d0bcd663fd4814538ea143cbbfbb7e862
Kyle Robinson Young <kyle@dontkry.com>
2013-07-24 14:28:03 -0700

    Remove color and texture id. We'll come back to that later

--- index.js
+++ index.js
@@ -17,10 +17,6 @@ module.exports = function(arr, geometry) {
       itemSize: 3,
       array: new Float32Array(triangles * 3 * 3)
     },
-    /*color: {
-      itemSize: 3,
-      array: new Float32Array( triangles * 3 * 3 )
-    },*/
   }
 
   var chunkSize = Math.floor(Math.pow(2, 16) / 3)
@@ -32,20 +28,15 @@ module.exports = function(arr, geometry) {
 
   var positions = geometry.attributes.position.array
   var normals = geometry.attributes.normal.array
-  //var colors = geometry.attributes.color.array
-
-  //var color = new THREE.Color(0xff0000)
 
   var p = 0
   for (var i = 0; i < vert_data.length; i += 8) {
     var x = vert_data[i + 0]
     var y = vert_data[i + 1]
     var z = vert_data[i + 2]
-    //var ao = vert_data[i + 3]
     var nx = vert_data[i + 4]
     var ny = vert_data[i + 5]
     var nz = vert_data[i + 6]
-    //var tid = vert_data[i + 7]
 
     positions[p + 0] = x
     positions[p + 1] = y
@@ -54,10 +45,6 @@ module.exports = function(arr, geometry) {
     normals[p + 0] = nx
     normals[p + 1] = ny
     normals[p + 2] = nz
-
-    //colors[p + 0] = color.r
-    //colors[p + 1] = color.g
-    //colors[p + 2] = color.b
 
     p += 3
   }
