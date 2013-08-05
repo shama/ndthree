4cf32729b2606bbc0085221cffd1ede9cae12565
Kyle Robinson Young <kyle@dontkry.com>
2013-08-05 08:51:01 -0700

    save

--- example.js
+++ example.js
@@ -3,7 +3,7 @@ var fill = require('ndarray-fill')
 var terrain = require('isabella-texture-pack')
 var THREE = require('three')
 
-var ndthree = require('./')
+var createND3 = require('./')
 
 var width = window.innerWidth
 var height = window.innerHeight
@@ -13,15 +13,50 @@ renderer.setClearColor(0xBFD1E5, 1.0)
 var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 100000)
 var scene = new THREE.Scene()
 scene.add(camera)
-camera.position.z = 60
+camera.position.z = 260
 renderer.setSize(width, height)
 
 // create tiles from tilemap
 var tiles = ndarray(terrain.data,
   [16,16,terrain.shape[0]>>4,terrain.shape[1]>>4,4],
   [terrain.stride[0]*16, terrain.stride[1]*16, terrain.stride[0], terrain.stride[1], terrain.stride[2]], 0)
-var material = new THREE.ShaderMaterial()
 
+var nd3 = createND3({
+  geometry: new THREE.BufferGeometry(),
+  material: new THREE.ShaderMaterial(),
+})
+var mesh = nd3.createMesh({
+  THREE: THREE,
+  map: tiles,
+  offset: [-16, -16, -16],
+})
+scene.add(mesh)
+
+function create(pos) {
+  var shape = [32, 32, 32]
+  var voxels = ndarray(new Uint16Array(shape[0]*shape[1]*shape[2]), shape)
+  fill(voxels, function(i,j,k) {
+    if (j === 0) return 1
+    var x = Math.abs(i - (shape[0]/2))
+    var y = Math.abs(j - (shape[1]/2))
+    var z = Math.abs(k - (shape[2]/2))
+    return (x*x+y*y+z*z) < 32 ? ((Math.random()*255)|0) + (1<<15) : 0
+  })
+  nd3.chunk(pos, voxels)
+}
+create([0,0,0])
+create([1,0,0])
+create([2,0,0])
+create([0,0,1])
+create([0,0,2])
+
+/*setInterval(function() {
+  console.log('change!')
+  create([0, 0, 0])
+}, 2000)*/
+
+
+/*
 var meshes = []
 function create(pos) {
   //Create some random voxels in a sphere
@@ -51,6 +86,7 @@ function create(pos) {
   return mesh
 }
 meshes.push(create([0, 0, 0]))
+*/
 
 var ambientLight = new THREE.AmbientLight(0xff0000)
 scene.add(ambientLight)
@@ -82,9 +118,7 @@ window.requestAnimFrame = (function(){
 }())
 
 function render() {
-  meshes.forEach(function(m) {
-    m.rotation.y += 0.01
-    m.rotation.z += 0.01
-  })
+  mesh.rotation.y += 0.01
+  mesh.rotation.z += 0.01
   renderer.render(scene, camera)
 }
--- index.js
+++ index.js
@@ -7,24 +7,17 @@ var fs = require('fs')
 var vsh = fs.readFileSync(__dirname + '/shaders/ao.vsh')
 var fsh = fs.readFileSync(__dirname + '/shaders/ao.fsh')
 
-module.exports = function(arr, geometry, material) {
-  var data = createAOMesh(arr)
-  if (data === null) return false
+function ND3(opts) {
+  opts = opts || {}
+  this.distance = opts.distance || 2
+  this.shape = opts.shape || [32, 32, 32]
+  this.geometry = opts.geometry || {}
+  this.material = opts.material || {}
 
-  extend(true, material, {
-    uniforms: {
-      tileSize: { type: 'f', value: 16.0 },
-      tileMap: { type: 't', value: [] },
-    },
-    attributes: {
-      attrib0: { type: 'v2', value: [] },
-    },
-    vertexShader: vsh,
-    fragmentShader: fsh,
-  })
+  // 12 triangles per cube * cubes per chunk
+  var triangles = 12 * this.shape[0] * this.shape[1] * this.shape[2]
 
-  var triangles = 12 * arr.shape[0] * arr.shape[1] * arr.shape[2]
-  geometry.attributes = {
+  this.geometry.attributes = {
     index: {
       itemSize: 1,
       array: new Uint16Array(triangles * 3)
@@ -42,18 +35,61 @@ module.exports = function(arr, geometry, material) {
       array: new Float32Array(triangles * 3 * 2)
     },
   }
+  //this.geometry.dynamic = true
+
+  this.material.uniforms = {
+    tileSize: { type: 'f', value: 16.0 },
+    tileMap: { type: 't', value: [] },
+  }
+  this.material.attributes = {
+    position: { type: 'v3', value: null },
+    normal: { type: 'v3', value: null },
+    attrib0: { type: 'v2', value: null },
+  }
+  this.material.vertexShader = vsh
+  this.material.fragmentShader = fsh
 
   var chunkSize = Math.floor(Math.pow(2, 16) / 3)
-
-  var indices = geometry.attributes.index.array
+  var indices = this.geometry.attributes.index.array
   for (var i = 0; i < indices.length; i++) {
     indices[i] = i % (3 * chunkSize)
   }
 
-  var positions = geometry.attributes.position.array
-  var normals = geometry.attributes.normal.array
-  var attrib0s = geometry.attributes.attrib0.array
+  this.geometry.offsets = []
+  var offsets = triangles / chunkSize
+  for (var i = 0; i < offsets; i++) {
+    var offset = {
+      start: i * chunkSize * 3,
+      index: i * chunkSize * 3,
+      count: Math.min(triangles - (i * chunkSize), chunkSize) * 3
+    };
+    this.geometry.offsets.push(offset)
+  }
+}
+module.exports = function(geometry, material) {
+  return new ND3(geometry, material)
+}
+module.exports.ND3 = ND3
 
+ND3.prototype.offset = function(pos) {
+}
+
+ND3.prototype.chunk = function(pos, arr) {
+  var self = this
+  var data = createAOMesh(arr)
+  if (data === null) return
+
+  var positions = this.geometry.attributes.position.array
+  var normals = this.geometry.attributes.normal.array
+  var attrib0s = this.geometry.attributes.attrib0.array
+
+  //var offset = 12 * pos[0] * this.shape[0] * pos[1] * this.shape[1] * pos[2] * this.shape[2]
+  var offset = 12 * (this.shape[0] * pos[0]) * (this.shape[1] * pos[1]) * (this.shape[2] * pos[2])
+  //offset *= pos[0] * pos[1] * pos[2]
+  //if (pos[0] !== 0) offset = offset * 2
+  console.log(pos, offset, this.shape)
+
+  //var p3 = offset * 3 * 3, p2 = offset * 3 * 2
   var p3 = 0, p2 = 0
   for (var i = 0; i < data.length; i += 8) {
     var x = data[i + 0], y = data[i + 1], z = data[i + 2], ao = data[i + 3]
@@ -62,9 +98,9 @@ module.exports = function(arr, geometry, material) {
     attrib0s[p2 + 0] = ao
     attrib0s[p2 + 1] = tid
 
-    positions[p3 + 0] = x
-    positions[p3 + 1] = y
-    positions[p3 + 2] = z
+    positions[p3 + 0] = x + (this.shape[0] * pos[0])
+    positions[p3 + 1] = y + (this.shape[1] * pos[1])
+    positions[p3 + 2] = z + (this.shape[2] * pos[2])
 
     normals[p3 + 0] = nx
     normals[p3 + 1] = ny
@@ -74,19 +110,8 @@ module.exports = function(arr, geometry, material) {
     p2 += 2
   }
 
-  geometry.offsets = []
-  var offsets = triangles / chunkSize
-  for (var i = 0; i < offsets; i++) {
-    var offset = {
-      start: i * chunkSize * 3,
-      index: i * chunkSize * 3,
-      count: Math.min(triangles - (i * chunkSize), chunkSize) * 3
-    };
-    geometry.offsets.push(offset)
-  }
-
-  Object.keys(geometry.attributes).forEach(function(key) {
-    geometry.attributes[key].needsUpdate = true
+  Object.keys(this.geometry.attributes).forEach(function(key) {
+    self.geometry.attributes[key].needsUpdate = true
   })
 }
 
@@ -96,10 +121,10 @@ function reshapeTileMap(tiles) {
 }
 
 // Helper for creating a mesh, this part requires THREE
-module.exports.createMesh = function(opts) {
+ND3.prototype.createMesh = function(opts) {
   var THREE = opts.THREE
-  var geometry = opts.geometry
-  var material = opts.material
+  var geometry = opts.geometry || this.geometry
+  var material = opts.material || this.material
   var texture = opts.map || opts.texture || false
   var offset = opts.offset || [1, 1, 1]
   var pad = opts.pad !== false
@@ -109,29 +134,31 @@ module.exports.createMesh = function(opts) {
 
   if (texture) {
     var pyramid = tileMipMap(texture, pad)
-    texture = reshapeTileMap(pyramid[0])
+    var map1 = reshapeTileMap(pyramid[0])
 
-    texture = new THREE.DataTexture(texture.data, texture.shape[1], texture.shape[0],
+    var map = new THREE.DataTexture(map1.data, map1.shape[1], map1.shape[0],
       THREE.RGBAFormat, THREE.UnsignedByteType, (new THREE.UVMapping()),
       THREE.RepeatWrapping, THREE.RepeatWrapping,
-      THREE.NearestFilter, THREE.NearestFilter)
-    texture.flipY = false
-    texture.wrapS = 1000
-    texture.wrapT = 1000
+      THREE.NearestFilter, THREE.NearestMipMapNearestFilter)
+    map.flipY = false
+    map.wrapS = 1000
+    map.wrapT = 1000
+    //map.generateMipmaps = false
+    //map.mipmaps[0] = {data: map1.data, width: map1.shape[1], height: map1.shape[0]}
 
     // set mipmap levels
-    for (var i = 1; i < pyramid.length; ++i) {
+    /*for (var i = 1; i < pyramid.length; ++i) {
       var level = reshapeTileMap(pyramid[i])
-      texture.mipmaps.push({
+      map.mipmaps.push({
         data: level.data,
         width: level.shape[1],
         height: level.shape[0],
       })
-    }
+    }*/
 
-    texture.needsUpdate = true
+    map.needsUpdate = true
 
-    material.uniforms.tileMap.value = texture
+    material.uniforms.tileMap.value = map
     material.side = THREE.DoubleSide
     material.needsUpdate = true
   }
